606abc4e96f5e54754e387d0badb39572f7646b6
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2024-01-13 16:22:33 +0800

    feat: intial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Contact Us</h1>
+      <p>
+        Need us to get in touch with you? Please fill out the form with your
+        inquiry and we will get back to you
+      </p>
+    </>
+  );
+}
+
+export default App;
