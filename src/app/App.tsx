3ad282a259e30b7760ba4436524a1503d3e2e3d4
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2024-02-06 19:27:45 +0800

    feat: add form handling with reach-hook-form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,64 @@
-import { FormEvent } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import "./App.css";
 
+type FormData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
 function App() {
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log("hello");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormData>();
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log("form values", data);
   };
   return (
     <section id="contact-us">
-      <div className="contact-us-text">
-        <h1>Contact Us</h1>
+      <div className="contact-us-heading">
+        <h1>How can we help you today?</h1>
         <p>
           Need us to get in touch with you? Please fill out the form with your
-          inquiry and we will get back to you
+          inquiry and we will get back to you.
         </p>
       </div>
-      <form className="contact-us-form" onSubmit={onSubmit}>
-        <div className="contact-us-form-field">
-          <label>Name</label>
-          <input type="text" name="name" />
+      <form className="contact-us-form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="contact-us-form">
+          <div className="contact-us-form-field">
+            <label>Name</label>
+            <input
+              {...register("name")}
+              type="text"
+              name="name"
+              placeholder="Enter your name..."
+            />
+          </div>
+          <div className="contact-us-form-field">
+            <label>Email</label>
+            <input
+              {...register("email", { required: true })}
+              type="email"
+              name="email"
+              placeholder="Enter your email..."
+            />
+            {errors.email?.type === "required" && (
+              <p role="alert">First name is required</p>
+            )}
+          </div>
+          <div className="contact-us-form-field">
+            <label>Message</label>
+            <textarea
+              {...register("message")}
+              placeholder="Type your message here.."
+              rows={4}
+              cols={30}
+            />
+          </div>
         </div>
-
-        <button type="submit">submit</button>
+        <button type="submit">Submit</button>
       </form>
     </section>
   );
